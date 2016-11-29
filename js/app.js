8269c586997b065b779628ac2d6f705f7097f75b
fabraga <fabraga@gmail.com>
2016-11-29 22:08:33 +0000

    update 4

--- js/app.js
+++ js/app.js
@@ -2,35 +2,105 @@
   'use strict';
 
   angular.module('myApp', [])
-  .controller('myController', myController);
+  .controller('CounterController', CounterController)
+  .controller('filterController', FilterController)
+  .controller('BindingController', BindingController)
+  .filter('altercase', AlternateCaseFilter);
+  //.filter('replacefield', ReplaceFilter);
 
-  myController.$inject = ['$scope'];
-  function myController ($scope) {
-    $scope.dishes = "";
-    $scope.message = "";
 
-    $scope.check = function() {
-      $scope.total = calculateDishes($scope.dishes);
-      if ($scope.total > 3) {
-        $scope.message = "Too much!";
-      }
-      else {
-        $scope.message = "Enjoy!";
-      }
+  CounterController.$inject = ['$scope', '$timeout'];
+  function CounterController($scope, $timeout) {
+    $scope.counter = 0;
+
+    $scope.upCount = function () {
+      $scope.counter++;
+    };
+
+    $scope.upDigest = function () {
+      setTimeout(function () {
+        $scope.counter++;
+        $scope.$digest();
+        console.log("with $digest (AngularJS wont see possible exceptions inside 'setTimeout')");
+      }, 800)
+    };
+
+    $scope.upApply = function () {
+      setTimeout(function () {
+        $scope.$apply(function () {
+          $scope.counter++;
+          console.log("with $apply (AngularJS catches possible exceptions inside 'setTimeout')");
+        });
+      }, 800)
+    };
+
+    $scope.upTimeout = function () {
+      $timeout(function () {
+        $scope.counter++;
+        console.log("$timeout is AngularJS native version for 'setTimeout'");
+      }, 800);
     }
+  }
 
-    function calculateDishes(string) {
-      var dishCount = 0;
-      if (string.length > 0) {
-        dishCount++;
-      }
-      for (var i = 0 ; i < string.length ; i++) {
-        if (string.charAt(i) === ",") {
-          dishCount ++;
+  FilterController.$inject = ['$scope', '$filter', 'altercaseFilter'];
+  function FilterController ($scope, $filter, altercaseFilter) {
+    $scope.product = "Albergine";
+    $scope.price = .55;
+    $scope.name = "Fabricio";
+
+    $scope.showPrice = function() {
+      return $filter('currency')($scope.price,'€');
+    };
+    $scope.capitalize = function() {
+      $scope.product = $filter('uppercase')($scope.product);
+    };
+    $scope.lowercase = function() {
+      $scope.product = $filter('lowercase')($scope.product);
+    };
+    $scope.altercase = function() {
+      $scope.product = altercaseFilter($scope.product);
+    };
+    // $scope.replacefield = function() {
+    //   $scope.product = replaceFilter($scope.product, $scope.name, $scope.product);
+    // }
+  }
+
+  function AlternateCaseFilter() {
+    return function (input) {
+      input = input || "";
+      var output = "";
+      for(var i=0; i<input.length; i++) {
+        if ( i%2 == 0) {
+          output += input.substring(i, i + 1).toUpperCase();
+        } else {
+          output += input.substring(i, i + 1).toLowerCase();
         }
       }
-      return dishCount;
+      return output;
+    };
+  }
+
+  // function ReplaceFilter() {
+  //   return function (input, target, replacement) {
+  //     input = input !! "";
+  //     input = input.replace(target, replacement);
+  //     return input;
+  //   };
+  // }
+
+  BindingController.$inject = ['$scope'];
+  function BindingController($scope) {
+    $scope.firstName = "Fabricio";
+
+    // Just return fullName string:
+    $scope.getFullName = function () {
+      return $scope.firstName + " " + "Braga";
     }
+    // Set fullName 1-time only: 
+    $scope.setFullName = function () {
+      $scope.fullName = $scope.firstName + " " + "Braga";
+    };
+
   }
 
 })();
